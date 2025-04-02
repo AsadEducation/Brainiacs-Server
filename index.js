cbf23b087ac13fe1105cdc531c367f9c23b05ed6
Hasibul Hasan <hasibul277127@gmail.com>
2025-04-02 06:48:41 +0600

    added column and task  get post and put api

--- index.js
+++ index.js
@@ -34,14 +34,87 @@ async function run() {
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
+
+    const database = client.db("Brainiacs");
     const userCollection = client.db("Brainiacs").collection("users");
-    
+    const columnCollection = database.collection("Columns");
+    const taskCollection = database.collection("Tasks");
+
     //user collection is empty now
     app.get("/users", async (req, res) => {
       const cursor = userCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
+
+
+
+    // task management board
+
+    //  column related apis 
+    app.get("/columns", async (req, res) => {
+      console.log("hit the column get");
+      const result = await columnCollection.find().sort({ order: 1 }).toArray();
+      res.send(result);
+    })
+    app.post("/columns", async (req, res) => {
+      console.log("hit the columns post api")
+      const column = req.body;
+      const newColumns = { ...column, order: column.length }
+      const result = await columnCollection.insertOne(newColumns);
+      res.send(result);
+    })
+
+    app.put("/columns", async (req, res) => {
+      console.log("hit the columns put api");
+      const columnSet = req.body;
+      console.log("columnSet:", columnSet)
+      const updateOperations = columnSet.map((column, index) => {
+        const { _id, ...columnData } = column;
+        columnCollection.updateOne(
+          { id: column.id },
+          { $set: { ...columnData, order: index } },
+          { upsert: true }
+        )
+      });
+      await Promise.all(updateOperations);
+
+      res.send({ message: "Tasks updated" });
+
+    });
+
+
+    // task related apis
+    app.get("/tasks", async (req, res) => {
+      const result = await taskCollection.find().sort({ order: 1 }).toArray();
+      res.send(result);
+    })
+    app.post("/tasks", async (req, res) => {
+      const task = req.body;
+      const newTask = { ...task }
+      const result = await taskCollection.insertOne(newTask);
+      res.send(result);
+    })
+    app.put("/tasks", async (req, res) => {
+      console.log("hit the task put api");
+      const taskSet = req.body;
+      console.log("taskSet:", taskSet)
+      const updateOperations = taskSet.map((task, index) => {
+        const { _id, ...taskData } = task;
+        taskCollection.updateOne(
+          { id: task.id },
+          { $set: { ...taskData, order: index } },
+          { upsert: true }
+        )
+      });
+      await Promise.all(updateOperations);
+
+      res.send({ message: "Tasks updated" });
+
+    })
+
+
+
   } finally {
   }
 }
